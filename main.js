dcbd6cd44f531cc56aff8788c2520c8178995a6d
TatianaKan <klanukt@gmail.com>
2021-10-14 13:11:45 +0300

    создана функция

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ const player1 = {
     hp: 100,
     img: 'http://reactmarathon-api.herokuapp.com/assets/scorpion.gif',
     weapon: ['knife', 'dagger'],
-    attack: function() {
-        console.log (player1 + 'Fight');
+    attack: function () {
+        console.log(player1 + 'Fight');
     }
 };
 
@@ -13,8 +13,8 @@ const player2 = {
     hp: 100,
     img: 'http://reactmarathon-api.herokuapp.com/assets/kitana.gif',
     weapon: ['knife', 'dagger'],
-    attack: function() {
-        console.log (player2 + 'Fight');
+    attack: function () {
+        console.log(player2 + 'Fight');
     }
 };
 
@@ -22,8 +22,37 @@ const player2 = {
 // console.log(player2);
 
 function createPlayer() {
+    const $root = document.querySelector('.root');
+
     const $player1 = document.createElement('div');
-    $player1.classList.add('player1')
+    $player1.classList.add('player1');
+
+    const $progressbar = document.createElement('div');
+    $progressbar.classList.add('progressbar');
+    const $character = document.createElement('div');
+    $character.classList.add('character');
+
+    $player1.appendChild($progressbar);
+    $player1.appendChild($character);
+
+
+    const $life = document.createElement('div');
+    $life.classList.add('life');
+    const $name = document.createElement('div');
+    $name.classList.add('name');
+
+    $progressbar.appendChild($life);
+    $progressbar.appendChild($name);
+
+    $name.innerText='SCORPION';
+
+    const $img = document.createElement('img');
+    $img.src='http://reactmarathon-api.herokuapp.com/assets/scorpion.gif';
+
+    $character.appendChild($img)
+
+
+    $root.appendChild($player1);
 }
 
 createPlayer();
